3a14ae5ff66c61f9438e54d978b6b6c222239555
Andrea Janatová <andrea.janatova@seznam.cz>
2018-01-20 17:57:47 +0100

    another revision
    blog/templates/blog/base.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views # . znamená aktuální adresář
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), # '^$' = regulární výraz, způsob, jak hledat řetězce; řetězce, které v sobě mají a -> 'a*', ^ znamená začátek, $ znamená konec
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
